b3ee8565d1221f9cd8200e01b1656039c1169418
wojciechprzyluski <przyluskiwojciech@gmail.com>
2025-03-23 14:19:07 +0100

    ref: extract nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,17 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
-import cn from 'classnames';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Home } from './components/Home/Home';
 import { TabsPage } from './components/TabsPage/TabsPage';
 import { PageNotFound } from './components/PageNotFound/PageNotFound';
 import { tabs } from './data/tabs';
+import { Nav } from './components/nav/Nav';
 
 export const App = () => {
   return (
     <>
-      {/* Also requires <html class="has-navbar-fixed-top"> */}
-      <nav
-        className="navbar is-light is-fixed-top is-mobile has-shadow"
-        data-cy="Nav"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <NavLink
-              end
-              to="/"
-              className={({ isActive }) =>
-                cn('navbar-item ', { 'is-active': isActive })
-              }
-            >
-              Home
-            </NavLink>
-            <NavLink
-              to="/tabs"
-              className={({ isActive }) =>
-                cn('navbar-item', { 'is-active': isActive })
-              }
-            >
-              Tabs
-            </NavLink>
-          </div>
-        </div>
-      </nav>
+      <Nav />
 
       <div className="section">
         <div className="container">
